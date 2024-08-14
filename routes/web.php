a1b31ba326244343aba87a02a1e16e34e8921a32
nyaberi1 <kjoshuan001@gmail.com>
2024-08-14 15:26:20 +0300

    session on progress

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,26 @@
 <?php
 
-use App\Http\Controllers\JobController;
-use Illuminate\Support\Facades\Route;
-use PHPUnit\Framework\MockObject\ReturnValueNotConfiguredException;
-use Illuminate\Support\Arr;
 use App\Models\Job;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegisterdUserController;
 use Symfony\Contracts\Service\Attribute\Required;
+use PHPUnit\Framework\MockObject\ReturnValueNotConfiguredException;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
+
+
+// Auth
+Route::get('/register', [RegisterdUserController:: class, 'create']);
+Route::post('/register', [RegisterdUserController:: class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
 
 
  
